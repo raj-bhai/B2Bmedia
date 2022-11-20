493008f0a2dfc8a8e83b2e7eee52e2b707473eae
Raj <raj.kiran@ensologic.com>
2022-11-20 13:57:21 +0530

    header responsive

--- components/Home/header.js
+++ components/Home/header.js
@@ -34,13 +34,12 @@ const Header = (props) => {
 
     return (
         <div className={`w-[100%] h-[80px] border-[0px] flex fixed z-[200] top-0` + props.className} >
-            <div className="w-[385px] h-[83px] border-[0px] ml-[97.5px] mt-[8px] " >
+            <div className="w-[385px] h-[83px] border-[0px] sm:ml-[97.5px] ml-[20px] sm:mt-[8px] mt-[20px] " >
                 <h1
-                    //  className="font-extrabold text-transparent text-[30px] bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600"
-                    className={"absolute  font-bold ml-[0px] mt-[-5px] text-[52px] text-transparent text-[30px] bg-clip-text " + Gradiants[logoColor]}
+                    className={"absolute  font-bold ml-[0px] mt-[-5px] sm:text-[52px] text-transparent text-[35px] bg-clip-text " + Gradiants[logoColor]}
                 >GrowGrip</h1>
             </div>
-            <div className="flex mt-[19.5px] ml-[150x] w-[800px] border-[0px] justify-between " >
+            <div className="flex mt-[19.5px] ml-[150x] sm:w-[800px] sm:visible invisible border-[0px] justify-between " >
                 <div className=" w-[80px] h-[60px] border-[0px] flex items-center justify-center " >
                     <h1 className={"text-[#fff] font-normal text-[18px] leading-[30px] cursor-pointer " + textHover} >Home</h1>
                 </div>
--- components/header.js
+++ components/header.js
@@ -7,7 +7,7 @@ const Header = (props) => {
             <div className="w-[20%] items start px-5" >
                 <h1 className="cursor-pointer text-[#fff] hover:text-yellow-200">B2bMedia</h1>
             </div>
-            <div className="flex justify-evenly w-[80%]" >
+            <div className="flex justify-evenly sm:w-[80%] " >
                 <h1 className="cursor-pointer text-[#fff] italic hover:text-yellow-200 text-base" >Home</h1>
                 <h1 className="cursor-pointer text-[#fff] italic hover:text-yellow-200 text-base" >Service</h1>
                 <h1 className="cursor-pointer text-[#fff] italic hover:text-yellow-200 text-base" >Portfolio</h1>
