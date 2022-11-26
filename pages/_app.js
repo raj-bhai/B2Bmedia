c681077228fcefa8f06751b720f87760deac71cc
Raj <raj.kiran@ensologic.com>
2022-11-26 17:40:17 +0530

    analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import '../styles/globals.css'
 import { Provider } from 'react-redux';
 import store from '../redux/store';
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
       <Component {...pageProps} />
+      <Analytics />
     </Provider>
   )
 }
